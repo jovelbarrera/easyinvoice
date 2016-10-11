ab2e00093d2a5fc9c4295064dd9c2ec28ce93a59
Roberto Jovel <rej.barrera@outlook.com>
2016-10-10 22:16:29 -0600

    Test

--- controllers/user/login.php
+++ controllers/user/login.php
@@ -2,7 +2,7 @@
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/invoicemaker/framework/config.php';
 require_once ROOT_PATH . '/utils/utils.inc.php';
-require_once ROOT_PATH . '/views/user/LoginView.php';
+/*require_once ROOT_PATH . '/views/user/LoginView.php';
 require_once ROOT_PATH . '/models/User.php';
 
 if (count($_POST) == 0) {
@@ -21,5 +21,7 @@ if (count($_POST) == 0) {
     } else {
         header('Location: ' . DOMAIN_NAME . '/index.php');
     }
-}
+}*/
+
+echo ROOT_PATH . '/utils/utils.inc.php';
 
