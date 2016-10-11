059c174d6cc28e4cc0e21cea0881cd30e0515b10
Roberto Jovel <rej.barrera@outlook.com>
2016-10-10 22:13:11 -0600

    Test

--- controllers/user/login.php
+++ controllers/user/login.php
@@ -1,25 +1,25 @@
 <?php
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/invoicemaker/framework/config.php';
-/* require_once ROOT_PATH . '/utils/utils.inc.php';
-  require_once ROOT_PATH . '/views/user/LoginView.php';
-  require_once ROOT_PATH . '/models/User.php';
+require_once ROOT_PATH . '/utils/utils.inc.php';
+require_once ROOT_PATH . '/views/user/LoginView.php';
+require_once ROOT_PATH . '/models/User.php';
 
-  if (count($_POST) == 0) {
-  $view = new LoginView(NULL);
-  $view->buildUI();
-  } else {
-  $data = array(
-  "username" => (string) secure_post('username', ''),
-  "password" => (string) secure_post('password', ''),
-  );
+if (count($_POST) == 0) {
+    $view = new LoginView(NULL);
+    $view->buildUI();
+} else {
+    $data = array(
+        "username" => (string) secure_post('username', ''),
+        "password" => (string) secure_post('password', ''),
+    );
 
-  $response = User::getInstance()->get_user_by_credentials($data);
-  if (isset($response['error'])) {
-  $view = new LoginView($response);
-  $view->buildUI();
-  } else {
-  header('Location: ' . DOMAIN_NAME . '/index.php');
-  }
-  } */
-echo ROOT_PATH;
+    $response = User::getInstance()->get_user_by_credentials($data);
+    if (isset($response['error'])) {
+        $view = new LoginView($response);
+        $view->buildUI();
+    } else {
+        header('Location: ' . DOMAIN_NAME . '/index.php');
+    }
+}
+
