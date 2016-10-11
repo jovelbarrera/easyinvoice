3d73d854fd2f526827ab6dbadc4d3eb96b37a584
Roberto Jovel <rej.barrera@outlook.com>
2016-10-10 22:59:26 -0600

    ready?

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once $_SERVER['DOCUMENT_ROOT'] . '/invoicemaker/framework/config.php';
+//echo 'Location: ' . DOMAIN_NAME . '/controllers/user/login.php';
 session_start();
 if (isset($_SESSION['logged'])) {
     header('Location: ' . DOMAIN_NAME . '/controllers/client/read.php');
