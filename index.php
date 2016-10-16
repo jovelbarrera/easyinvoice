9e487fb5ced86c7bbb75dd49ab1c300776d0d0d3
Roberto Jovel <rej.barrera@outlook.com>
2016-10-16 01:15:29 -0600

    JB framework

--- index.php
+++ index.php
@@ -8,7 +8,11 @@ $files2 = scandir($dir, 1);
 
 print_r($files1);
 print_r($files2);
-require_once ('framework/loader.php');
+try {
+    require_once ('framework/loader.php');
+} catch (Exception $ex) {
+    $ex->getMessage();
+}
 //require_once (__DIR__ . '/framework/loader.php');
 //require_once (__DIR__ . '/framework/Dispatcher.php');
 
@@ -16,8 +20,6 @@ $controller = isset($_GET['controller']) ? $_GET['controller'] : "home";
 $action = isset($_GET['action']) ? $_GET['action'] : "index";
 $raw_parameters = isset($_GET['parameters']) ? $_GET['parameters'] : "";
 $parameters = $raw_parameters == "" ? array() : explode("/", $raw_parameters);
-print_r($controller);
-print_r($action);
-print_r($parameters);
+
 $dispatcher = new Dispatcher();
 $dispatcher->handle($controller, $action, $parameters);
