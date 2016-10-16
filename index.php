a6c4ec34cff923f0e837415584445672bb095abc
Roberto Jovel <rej.barrera@outlook.com>
2016-10-16 01:00:52 -0600

    JB framework

--- index.php
+++ index.php
@@ -10,6 +10,6 @@ $controller = isset($_GET['controller']) ? $_GET['controller'] : "home";
 $action = isset($_GET['action']) ? $_GET['action'] : "index";
 $raw_parameters = isset($_GET['parameters']) ? $_GET['parameters'] : "";
 $parameters = $raw_parameters == "" ? array() : explode("/", $raw_parameters);
-
+print_r($raw_parameters);
 $dispatcher = new Dispatcher();
 $dispatcher->handle($controller, $action, $parameters);
