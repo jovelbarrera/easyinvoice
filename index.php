7015361acc162bf9f4f282f651ca6650309680cf
Roberto Jovel <rej.barrera@outlook.com>
2016-10-16 01:31:31 -0600

    JB framework

--- index.php
+++ index.php
@@ -1,29 +1,20 @@
 <?php
 
-//use framework\Dispatcher;
-echo __DIR__."<br>";
-$dir = __DIR__;
-$files1 = scandir($dir);
-$files2 = scandir($dir, 1);
+use framework\Dispatcher;
 
-echo '<pre>';
-print_r($files1);
-print_r($files2);
-echo '</pre>';
-
-echo " Existe: " . file_exists (__DIR__.'/framework/loader.php');
 try {
-    require_once ('./framework/loader.php');
-} catch (Exception $ex) {
-    echo $ex->getMessage();
-}
+    require_once (__DIR__ . '/framework/loader.php');
+
 //require_once (__DIR__ . '/framework/loader.php');
 //require_once (__DIR__ . '/framework/Dispatcher.php');
 
-$controller = isset($_GET['controller']) ? $_GET['controller'] : "home";
-$action = isset($_GET['action']) ? $_GET['action'] : "index";
-$raw_parameters = isset($_GET['parameters']) ? $_GET['parameters'] : "";
-$parameters = $raw_parameters == "" ? array() : explode("/", $raw_parameters);
+    $controller = isset($_GET['controller']) ? $_GET['controller'] : "home";
+    $action = isset($_GET['action']) ? $_GET['action'] : "index";
+    $raw_parameters = isset($_GET['parameters']) ? $_GET['parameters'] : "";
+    $parameters = $raw_parameters == "" ? array() : explode("/", $raw_parameters);
 
-$dispatcher = new Dispatcher();
-$dispatcher->handle($controller, $action, $parameters);
+    $dispatcher = new Dispatcher();
+    $dispatcher->handle($controller, $action, $parameters);
+} catch (Exception $ex) {
+    echo $ex->getMessage();
+}
