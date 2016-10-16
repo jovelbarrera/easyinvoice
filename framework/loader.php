af569c59600b16fad4406cf470d297d7f6d6a983
Roberto Jovel <rej.barrera@outlook.com>
2016-10-16 02:07:55 -0600

    JB framework

--- framework/loader.php
+++ framework/loader.php
@@ -2,8 +2,7 @@
 
 spl_autoload_register(function ($class_name) {
     try {
-        require_once ($class_name);
-        echo $class_name;
+        require_once ($_SERVER['DOCUMENT_ROOT'] . "/" . $class_name);
     } catch (Exception $ex) {
         //echo $ex->getMessage();
         echo 'spl_autoload_register';
