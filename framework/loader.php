677f1da1b0a0cd8273e24984b23ebfe42386a1ca
Roberto Jovel <rej.barrera@outlook.com>
2016-10-17 23:13:11 -0600

    JB framework

--- framework/loader.php
+++ framework/loader.php
@@ -18,7 +18,7 @@ spl_autoload_register(function ($class_name) {
             }
             $file_name .= str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
         }
-        echo $file_name;
+        echo $_SERVER['DOCUMENT_ROOT'] . '/easyinvoice/' .$file_name;
         require_once ($_SERVER['DOCUMENT_ROOT'] . '/easyinvoice/' . $file_name);
     } catch (Exception $ex) {
         //echo $ex->getMessage();
