2f752e4de710de8d2ebfac2b50678106e6753967
Roberto Jovel <rej.barrera@outlook.com>
2016-10-17 23:35:10 -0600

    JB framework

--- framework/loader.php
+++ framework/loader.php
@@ -8,18 +8,18 @@ spl_autoload_register(function ($class_name) {
 //        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
 //            $file_name = "\\" . $class_name;
 //        } else {
-            $class_name = ltrim($class_name, '\\');
-            $file_name = '';
-            $namespace = '';
-            if ($lastNsPos = strrpos($class_name, '\\')) {
-                $namespace = substr($class_name, 0, $lastNsPos);
-                $class_name = substr($class_name, $lastNsPos + 1);
-                $file_name = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
-            }
-            $file_name .= str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
+        $class_name = ltrim($class_name, '\\');
+        $file_name = '';
+        $namespace = '';
+        if ($lastNsPos = strrpos($class_name, '\\')) {
+            $namespace = substr($class_name, 0, $lastNsPos);
+            $class_name = substr($class_name, $lastNsPos + 1);
+            $file_name = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+        }
+        $file_name .= str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
 //        }
         echo $_SERVER['DOCUMENT_ROOT'] . '/easyinvoice/' . $file_name;
-        require_once ( $file_name);
+        require_once ( strtolower($file_name));
     } catch (Exception $ex) {
         //echo $ex->getMessage();
         echo 'spl_autoload_register';
