172c047cbcf7bd201f84806a5916fb63d1fc1120
Roberto Jovel <rej.barrera@outlook.com>
2016-10-17 23:07:26 -0600

    JB framework

--- framework/loader.php
+++ framework/loader.php
@@ -5,13 +5,21 @@ namespace framework;
 spl_autoload_register(function ($class_name) {
     try {
 
-        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
-            $include = "\\" . $class_name;
-        } else {
-            $include = strtolower(str_replace("\\", "/", $class_name));
-        }
-        echo $include;
-        spl_autoload($include);
+//        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+//            $file_name = "\\" . $class_name;
+//        } else {
+            $class_name = ltrim($class_name, '\\');
+            $file_name = '';
+            $namespace = '';
+            if ($lastNsPos = strrpos($class_name, '\\')) {
+                $namespace = substr($class_name, 0, $lastNsPos);
+                $class_name = substr($class_name, $lastNsPos + 1);
+                $file_name = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+            }
+            $file_name .= str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
+//        }
+        echo $file_name;
+        require_once ($file_name);
     } catch (Exception $ex) {
         //echo $ex->getMessage();
         echo 'spl_autoload_register';
