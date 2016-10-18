272ab96e7f79eb4177d0e385f5e096a2ae5c2b31
Roberto Jovel <rej.barrera@outlook.com>
2016-10-17 22:07:21 -0600

    JB framework

--- framework/loader.php
+++ framework/loader.php
@@ -3,7 +3,11 @@
 spl_autoload_register(function ($class_name) {
     try {
         echo $class_name;
-        spl_autoload($class_name);
+        if (strtoupper(substr(PHP_OS, 0, 3)) === 'WIN') {
+            spl_autoload($class_name);
+        } else {
+            spl_autoload(strtolower(str_replace("\\", "/", $class_name)));
+        }
     } catch (Exception $ex) {
         //echo $ex->getMessage();
         echo 'spl_autoload_register';
