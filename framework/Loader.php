7d6818deae1191e5766ef6384e9a850d9aeb98fb
Roberto Jovel <rej.barrera@outlook.com>
2016-10-19 21:27:22 -0600

    JB Framework

--- framework/Loader.php
+++ framework/Loader.php
@@ -12,11 +12,11 @@ spl_autoload_register(function ($class_name) {
             $class_name = substr($class_name, $lastNsPos + 1);
             $file_name = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
         }
-        //$file_name .= str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
+//        $file_name .= str_replace('_', DIRECTORY_SEPARATOR, $class_name) . '.php';
         $file_name .= $class_name . '.php';
 
         $full_path = $_SERVER['DOCUMENT_ROOT'] . '/easyinvoice/' . $file_name;
-        echo $full_path." existe:".file_exists($full_path)."- ";
+//        echo $full_path." existe:".file_exists($full_path)."- ";
         require_once ($full_path);
     } catch (Exception $ex) {
         echo $ex->getMessage();
